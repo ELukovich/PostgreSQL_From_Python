af58055d63b2ceb4b93aa77502f088b9921ce68a
Ekaterina Lukovich <elukovich@mail.ru>
2023-09-24 16:26:32 +0300

    add files

---
+++ main.py
@@ -0,0 +1,139 @@
+import psycopg2
+
+# удаление таблиц
+# conn = psycopg2.connect(database='post_f_pyth_db', user='postgres', password=open("password.md").read())
+# with conn.cursor() as cur:
+#     cur.execute("""
+#         DROP TABLE Clients CASCADE;
+#         DROP TABLE Phone_Clients;
+#     """)
+
+# создание таблиц
+def create_db(conn):
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Clients(
+            Clients_id INTEGER UNIQUE PRIMARY KEY,
+            first_name VARCHAR(40),
+            last_name VARCHAR(40),
+            email VARCHAR(40)
+    );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS Phone_Clients(
+            id SERIAL PRIMARY KEY,
+            Clients_id INTEGER NOT NULL REFERENCES Clients(Clients_id),
+            phone_number VARCHAR(12) UNIQUE
+    );
+    """)
+    conn.commit()
+
+# наполнение таблиц
+def add_client(conn, Clients_id, first_name, last_name, email, phone_number=None):
+    cur = conn.cursor()
+    cur.execute("""
+        INSERT INTO Clients (Clients_id, first_name, last_name, email) VALUES (%s, %s, %s, %s);
+        """, (Clients_id, first_name, last_name, email))
+    conn.commit()
+    cur.execute("""
+        SELECT * FROM Clients;
+        """)
+    print(cur.fetchall())
+
+    cur.execute("""
+        INSERT INTO Phone_Clients (Clients_id, phone_number) VALUES (%s, %s);
+        """, (Clients_id, phone_number))
+    conn.commit()
+    cur.execute("""
+        SELECT * FROM Phone_Clients;
+        """)
+    print(cur.fetchall())
+
+def add_phone(conn, Clients_id, phone_number):  # добавление телефона существующему клиенту
+    cur = conn.cursor()
+    cur.execute("""
+        UPDATE Phone_Clients SET phone_number=%s WHERE Clients_id=%s;
+        """, (phone_number, Clients_id))
+    conn.commit()
+
+# изменение данных
+def change_client(conn, Clients_id, first_name=None, last_name=None, email=None, phone_number=None):
+    cur = conn.cursor()
+    cur.execute("""
+        UPDATE Clients SET first_name=%s, last_name=%s, email=%s WHERE Clients_id=%s;
+        """, (first_name, last_name, email, Clients_id))
+    cur.execute("""
+        SELECT * FROM Clients;
+        """)
+    print(cur.fetchall())
+    cur.execute("""
+        UPDATE Phone_Clients SET phone_number=%s WHERE Clients_id=%s;
+        """, (phone_number, Clients_id))
+    cur.execute("""
+            SELECT * FROM Phone_Clients;
+            """)
+    print(cur.fetchall())
+
+# Удаление телефона у клиента
+def delete_phone(conn, Clients_id):
+    cur = conn.cursor()
+    cur.execute("""
+        UPDATE Phone_Clients SET phone_number=%s WHERE Clients_id=%s;
+        """, ('Null', Clients_id))
+    cur.execute("""
+        SELECT * FROM Phone_Clients;
+        """)
+    print(cur.fetchall())
+
+# удаление клиента
+def delete_client(conn, Clients_id):
+    # сначала удаляем клиента из таблицы с телефонами
+    cur = conn.cursor()
+    cur.execute("""
+        DELETE FROM Phone_Clients WHERE Clients_id=%s;
+        """, (Clients_id,))
+    cur.execute("""
+        SELECT * FROM Phone_Clients;
+        """)
+    print(cur.fetchall())
+
+    # теперь удаляем самого клиента
+    cur = conn.cursor()
+    cur.execute("""
+        DELETE FROM Clients WHERE Clients_id=%s;
+        """, (Clients_id,))
+    cur.execute("""
+        SELECT * FROM Clients;
+        """)
+    print(cur.fetchall())
+
+# поиск клиента
+def find_client(conn, first_name=None, last_name=None, email=None, phone_number=None):
+    cur = conn.cursor()
+    cur.execute("""
+    SELECT * FROM Clients c JOIN Phone_Clients p ON c.Clients_id = p.Clients_id WHERE first_name=%s OR last_name=%s
+    OR email=%s OR p.phone_number=%s;
+    """, (first_name, last_name, email, phone_number))
+    print(cur.fetchall())
+
+conn = psycopg2.connect(database='post_f_pyth_db', user='postgres', password=open("password.md").read())
+with conn.cursor() as cur:
+    create_db(conn)
+    add_client(conn, 1, 'Иван', 'Иванов', 'i.ivanov@i.ru', '+79031111111')
+    add_client(conn, 2, 'Петр', 'Петров', 'p.petrov@i.ru', '+79032222222')
+    add_client(conn, 3, 'Максим', 'Сидоров', 'm.sidorov@i.com')
+    add_client(conn, 4, 'Яков', 'Яковлев', 'ya.yakovlev@i.ru')
+    add_client(conn, 5, 'Степан', 'Степанов', 's.stepanov@i.ru', '+79033333333')
+    add_phone(conn, 2, '+79034444444')
+    add_phone(conn, 4, '+79035555555')
+    change_client(conn, 1, 'Иванушка', 'Иванов', 'si.ivanov@i.ru', '+79031111111')
+    change_client(conn, 4, 'Костя', 'Костяков', 'k.kostyakov@i.ru', '+79035555555')
+    delete_phone(conn, 1)
+    delete_client(conn, 4)
+    find_client(conn, first_name='Иванушка')
+    find_client(conn, last_name='Яковлев')
+    find_client(conn, email='s.stepanov@i.ru')
+    find_client(conn, phone_number='+79032222222')
+
+conn.close()
